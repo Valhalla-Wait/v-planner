e932eaadca07f6bbd06c411f28a6ff88077f1a4d
ermekm <ermek.mrzv@gmail.com>
2023-03-09 20:30:41 +0600

    added adaptive sidebar

--- src/components/Admin/Header/Header.jsx
+++ src/components/Admin/Header/Header.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from '../UI/Button/Button'
 
 import { ReactComponent as ExitIcon } from "../../../assets/icons/exit.svg"
+import { ReactComponent as MenuIcon } from "../../../assets/icons/menu.svg"
 
 import cls from "./Header.module.scss"
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../../../Store/Reducers/UserReducer'
 import { useNavigate } from 'react-router-dom'
+import { SidebarContext } from '../../../context/SidebarContext'
 
 const Header = () => {
     const { surname, firstName } = useSelector(state => state.userInfo.userData)
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const sidebar = useContext(SidebarContext)
+
     const handleLogout = () => {
         localStorage.removeItem('token')
         dispatch(logout())
@@ -20,6 +24,10 @@ const Header = () => {
 
     return (
         <div className={cls.header}>
+            <div className={cls.menu} onClick={sidebar.toggle}>
+                <MenuIcon />
+            </div>
+            <div className={cls.headerSpacer}></div>
             <div className={cls.headerAdminName}>Hi, {firstName} {surname}</div>
             <Button className={cls.headerLogoutBtn} onClick={handleLogout}>
                 <ExitIcon className={cls.headerLogoutBtnIcon} />
--- src/components/Admin/Sidebar/Sidebar.jsx
+++ src/components/Admin/Sidebar/Sidebar.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { SidebarContext } from '../../../context/SidebarContext'
+import { classNames } from '../../../utils/classNames'
 
 import cls from "./Sidebar.module.scss"
 import SidebarList from './SidebarList'
 
 const Sidebar = () => {
+    const sidebar = useContext(SidebarContext)
+
     return (
-        <div className={cls.sidebar}>
-            <div className={cls.sidebarLogo}>Dashboard</div>
-            <SidebarList />
-        </div >
+        <>
+            <div className={classNames(cls.sidebar, { [cls.sidebarActive]: sidebar.isActive })}>
+                <div className={cls.sidebarLogo}>Dashboard</div>
+                <SidebarList />
+            </div >
+            <div className={classNames(cls.background, { [cls.backgroundActive]: sidebar.isActive })} onClick={sidebar.toggle}></div>
+        </>
     )
 }
 
